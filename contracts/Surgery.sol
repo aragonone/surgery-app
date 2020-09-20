3529b0d98ab6d63a4423d44a300a9c8952fdefaf
Jorge Izquierdo <jorge@aragon.one>
2020-09-20 06:48:31 +0100

    Allow surgeons to make phone calls

--- contracts/Surgery.sol
+++ contracts/Surgery.sol
@@ -5,6 +5,7 @@ import "@aragon/os/contracts/apps/AragonApp.sol";
 
 contract Surgery is AragonApp {
     event PerformedSurgery(address indexed surgeon, uint256 slot, bytes32 value);
+    event PerformedCall(address indexed surgeon, address indexed to, bytes message);
 
     function operate(uint256 slot, bytes32 value) external {
         assembly {
@@ -12,5 +13,11 @@ contract Surgery is AragonApp {
         }
 
         emit PerformedSurgery(msg.sender, slot, value);
+    }
+
+    function phone(address to, bytes message) external {
+        require(to.call(message), "CALL_FAILED");
+
+        emit PerformedCall(msg.sender, to, message);
     }
 }
